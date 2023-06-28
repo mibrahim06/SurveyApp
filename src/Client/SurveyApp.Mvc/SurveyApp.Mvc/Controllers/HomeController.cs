a948b1c6f6c2695d3c343f883f007c50b419ce1f
Musibra <mustafaibrahimaksuu@gmail.com>
2023-06-28 11:50:03 +0300

    created survey controller and some ui improvements

--- src/Client/SurveyApp.Mvc/SurveyApp.Mvc/Controllers/HomeController.cs
+++ src/Client/SurveyApp.Mvc/SurveyApp.Mvc/Controllers/HomeController.cs
@@ -19,10 +19,7 @@ public class HomeController : Controller
         return View();
     }
 
-    public IActionResult Privacy()
-    {
-        return View();
-    }
+  
 
     [Authorize]
     public IActionResult Secured()
---
+++ src/Client/SurveyApp.Mvc/SurveyApp.Mvc/Controllers/SurveyController.cs
@@ -0,0 +1,20 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+
+namespace SurveyApp.Mvc.Controllers;
+
+public class SurveyController : Controller
+{
+
+    [Authorize]
+    public IActionResult MySurveys()
+    {
+        return View();
+    }
+    [Authorize]
+    public IActionResult CreateSurvey()
+    {
+        return View();
+    }
+}
